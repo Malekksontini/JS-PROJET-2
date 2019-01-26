287869b8cf06d771eb99b384b0e490756559ceed
Malekksontini <46626825+Malekksontini@users.noreply.github.com>
2019-01-26 15:16:50 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+var a = prompt ("ECRIVEZ VOTRE TEXTE ICI");
+alert (a)
+function souli (){
+    document.getElementById("txt").style.textDecoration="underline"
+}
+function gras (){
+    document.getElementById("txt").style.fontWeight="bold"
+};
+function ital (){
+    document.getElementById("txt").style.fontStyle="italic"
+};
